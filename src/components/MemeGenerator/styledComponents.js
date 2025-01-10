59158abc289ddf5b67c0521c6ad6b42c09a4eac7
kr2011la@gmail.com <kr2011la@gmail.com>
2025-01-10 13:55:01 +0000

    meme commited

---
+++ src/components/MemeGenerator/styledComponents.js
@@ -0,0 +1,73 @@
+// Style your components here
+import styled from 'styled-components/macro'
+
+export const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`
+
+export const MemeGeneratorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 550px;
+  padding-top: 32px;
+  padding-bottom: 32px;
+  padding-right: 24px;
+  padding-left: 24px;
+  @media screen and (min-width: 768px) {
+    width: 85%;
+    max-width: 1140px;
+  }
+`
+
+export const Heading = styled.h1`
+  color: #35469c;
+  font-family: 'Open Sans';
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 16px;
+  @media screen and (min-width: 768px) {
+    font-size: 36px;
+    text-align: left;
+  }
+`
+export const FormAndMemeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  @media screen and (min-width: 768px) {
+    flex-direction: row-reverse;
+  }
+`
+export const MemeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-image: url(${props => props.backgroundImage});
+`
+
+export const TextContent = styled.p`
+  color: #ffffff;
+  font-size: ${props => props.activeFontSizeId}px;
+  font-weight: 600;
+  font-family: 'Open Sans';
+`
+export const MemeGeneratorForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  @media screen and (min-width: 768px) {
+    width: 40%;
+  }
+`
+export const CustomLabel = styled.label``
+
+export const CustomInput = styled.input``
+export const CustomSelect = styled.select``
+
+export const CustomOption = styled.option``
+export const GenerateButton = styled.button``
